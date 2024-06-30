58dbdd049cc49f08454a1290309e81bec3710cc8
Matt H <mhh129@gmail.com>
2024-06-30 16:49:32 -0400

    Change pom.xml maving.compiler.source from 1.7 to 22, create App.java handleRequest method, update unit tests

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
@@ -16,5 +17,14 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void shouldReturnHelloWorld()
+    {
+        assertEquals(
+            "Hello, AWS Lambda!", 
+            new App().handleRequest()
+        );
     }
 }
